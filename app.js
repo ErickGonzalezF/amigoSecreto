deb5f42621d72ccf03ef4a139c32e8fcfbdb873f
ErickGonzalezF <erickgf9119@gmail.com>
2025-03-18 19:54:01 -0600

    juego terminado

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById('amigo');
+    let valor = input.value.trim();
+    
+    if (valor === '') {
+        alert('Ingresar un Nombre Válido');
+    } else {
+        amigos.push(valor);
+        input.value = "";
+        actualizarLista();
+    }
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.innerHTML = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo (){
+    if (amigos == "") {
+        alert("agrega primero amigos")
+    } else {
+        let indiceAleatorio = Math.floor(Math.random()*amigos.length)
+        document.getElementById("resultado").innerHTML = amigos[indiceAleatorio]
+    }
+}
